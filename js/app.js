bcf2d94d28319f262bc9c5d64cb982afe4950eec
Christos Bacharakis <christos@bacharakis.com>
2016-03-10 11:52:14 +0200

    Chagned menu label

--- js/app.js
+++ js/app.js
@@ -5,7 +5,7 @@ var myLink = this,
     myelement = $(this).attr("rel");
 $(myelement).slideToggle('normal', function() {
     $(myLink).text(
-        $(this).is(':visible') ? "Less Modules -" : "More Modules +"
+        $(this).is(':visible') ? "Less Modules <<" : "More Modules >>"
     );
 });
 });
