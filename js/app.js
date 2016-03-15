9ea3e9489e9d85c483c4b121c1f643557ecd70a0
Christos Bacharakis <christos@bacharakis.com>
2016-03-15 11:19:30 +0200

    Add dropdown menu into a button

--- js/app.js
+++ js/app.js
@@ -4,8 +4,6 @@ $("a.moreExpand").click(function(){
 var myLink = this,
     myelement = $(this).attr("rel");
 $(myelement).slideToggle('normal', function() {
-    $(myLink).text(
-        $(this).is(':visible') ? "Less Modules <<" : "More Modules >>"
-    );
+    
 });
 });
