7fba6e3d39d8c9dde97dafbba620d7b141ff69f9
Christos Bacharakis <christos@bacharakis.com>
2016-03-16 11:05:19 +0200

    Change menu view and behavior

--- js/app.js
+++ js/app.js
@@ -1,12 +1,1 @@
 $(document).foundation();
-$("#aboutExpand").hide();
-$("a.moreExpand").click(function(){
-var myLink = this,
-    myelement = $(this).attr("rel");
-$(myelement).slideToggle('normal', function() {
-    $(myLink).text(
-        $(this).is(':visible') ? "Less Modules" : "More Modules"
-);
-
-});
-});
